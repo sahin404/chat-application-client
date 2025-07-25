36c1cd7e08adf8d3de8da17e2bb0ec082e4ff915
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-25 18:31:29 +0600

    Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import Navbar from "./components/Navbar";
+import {Routes, Route} from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+
 const App = () => {
   return (
     <div className="text-red-500">
-      App
+      <Navbar></Navbar>
+      
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="/profile" element={<Profile></Profile>}></Route>
+        <Route path="/settings" element={<Settings></Settings>}></Route>
+      </Routes>
       
     </div>
   );
