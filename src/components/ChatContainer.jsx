f8e34675130fe2398bdfcfc0c0a09699aaa40931
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-28 21:36:35 +0600

    chat container done

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -3,10 +3,13 @@ import MessageSkeleton from "../components/skeletons/MessageSkeleton";
 import { useEffect } from "react";
 import ChatHeader from "../components/ChatHeader";
 import MessageInput from "../components/MessageInput";
+import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utility";
 
 const ChatContainer = () => {
   const { messages, getSelectedUserMessages, isMessagesLoading, selectedUser } =
     useChatStore();
+  const { user } = useAuthStore();
 
   useEffect(() => {
     getSelectedUserMessages(selectedUser._id);
@@ -24,7 +27,47 @@ const ChatContainer = () => {
   return (
     <div className="flex flex-1 flex-col overflow-auto">
       <ChatHeader></ChatHeader>
-      <p>Messages....</p>
+      {/* Messages Container */}
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${
+              message.SenderId === selectedUser._id ? "chat-start" : "chat-end"
+            }`}
+          >
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={`${
+                    message.SenderId == selectedUser._id
+                      ? selectedUser.profilePic || "avatar.png"
+                      : user.profilePic || "avatar.png"
+                  }`}
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="chat-header mb-1">
+                <time className="text-xs opacity-50 ml-1">
+                  {formatMessageTime(message.createdAt)}
+                </time>
+              </div>
+              <div className="chat-bubble flex flex-col">
+                {message.image && (
+                  <img
+                    src={message.image}
+                    alt="Attachment"
+                    className="sm:max-w-[200px] rounded-md mb-2"
+                  />
+                )}
+                {message.text && <p>{message.text}</p>}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
       <MessageInput></MessageInput>
     </div>
   );
