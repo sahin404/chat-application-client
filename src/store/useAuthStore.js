62508948761c97f3e9e4988fb33323cff2c17f05
Md. Sahin Alam <122547284+sahin404@users.noreply.github.com>
2025-07-30 20:07:14 +0600

    Update useAuthStore.js

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BaseURL = "http://localhost:3000";
+const BaseURL = "https://chat-application-server-1-jsm8.onrender.com/";
 
 export const useAuthStore = create((set, get) => ({
   user: null,
