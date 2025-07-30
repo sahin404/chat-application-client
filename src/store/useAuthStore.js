9aa9f28fd06df0106a1aa2b0798c2e3c3cb5a99f
Md. Sahin Alam <122547284+sahin404@users.noreply.github.com>
2025-07-31 00:07:23 +0600

    Update useAuthStore.js

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BaseURL = "http://localhost:3000/api";
+const BaseURL = "http://localhost:3000";
 
 export const useAuthStore = create((set, get) => ({
   user: null,
